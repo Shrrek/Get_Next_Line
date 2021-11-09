bb35b4fa2b8bfcac37a5823d24f15a0326167bf5
Shrrek <85354673+Shrrek@users.noreply.github.com>
2021-11-09 19:04:37 +0100

    Add files via upload

--- incs/get_next_line.h
+++ incs/get_next_line.h
@@ -6,15 +6,23 @@
 /*   By: jperales <jperales@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 16:37:07 by jperales          #+#    #+#             */
-/*   Updated: 2021/10/27 18:02:11 by jperales         ###   ########.fr       */
+/*   Updated: 2021/11/09 14:07:54 by jperales         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+
 char	*get_next_line(int fd);
 char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *str, int c);
+size_t	ft_strlen(char *s);
+
 
 
 
